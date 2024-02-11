f710c1dd9e687d84dceef20e045fe8a1248e97de
Ignacio Oyarzabal <ignaoya93@gmail.com>
2024-02-11 11:45:32 -0300

    Working home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'weather/:id',
+        component: WeatherComponent,
+        title: 'Weather'
+    }
+];
+
+export default routes;
